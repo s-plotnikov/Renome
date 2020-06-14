5b1e56048582228c2b916b74f404084b15fa6ee5
s-plotnikov <seryoga.22@mail.ru>
2020-06-14 15:08:31 +0700

    added header, body, footer

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,25 @@
 $(function () {
 
+    $('.slider__inner').slick({
+        dots: false,
+        infinite: true,
+        speed: 300,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        slidesToShow: 1,
+        adaptiveHeight: true,
+        arrows: true,
+        prevArrow: '<button class="slick-arrow slick-prev"><img src="../images/slider-left.png" alt=""></button>',
+        nextArrow: '<button class="slick-arrow slick-next"><img src="../images/slider-right.png" alt=""></button>',
+      });
+
+      $('.shop__slider').slick({
+        dots: false,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+        prevArrow: '<button class="slick-arrow slick-prev"><img src="../images/shop_slider/arrow-left.png" alt=""></button>',
+        nextArrow: '<button class="slick-arrow slick-next"><img src="../images/shop_slider/arrow-right.png" alt=""></button>',
+      });
 });
